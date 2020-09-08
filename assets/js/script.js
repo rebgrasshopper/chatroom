d5c363a571b4866f47874b10307c485172f029d6
Plover Brown <rellwoos@gmail.com>
2020-09-07 17:31:36 -0700

    finished implementing look and say keywords

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,31 +10,9 @@ const pubnub = new PubNub({
 });
 
 let channel;
-let locations = {
-  "North-Woods-Entrance": {exits:
-  {n:"North-Woods-Path-A", s:"Plains-X", w:"none", e:"none"},
-  descriptions:{light:"The path into the woods is well lit by sun from the plains to the south. Large redwoods and a variety of other trees competing for the leftover scraps of sunlight seem to stretch infinitely to either side, but the path to the north is clear. It's almost as if a diving line exists that keeps the plains on one side and the forest on the other.", dark:"You can just make out the way forward between the dense growth of trees with the faint glow of moonlight filtering dimly into the forest entrance."},
-  },
-  "North-Woods-Path-A":{exits:
-  {n:"North-Woods-Path-B", s:"North-Woods-Entrance", w:"none", e:"none"},
-  descriptions:{light:"The interior of the forest is shady, but well lit with dappled sunshine falling through the canopy. Birds call out to each other, unseen in the high tree-tops. You see the path stretching north before you.", dark:"It's almost pitch-black inside the wood, but you can just make out the path in front of you. The forest rustles around you in the night breeze, and the soft call of an owl floats in from far away."},
-  },
-  "North-Woods-Path-B":{exits:
-  {n:"Empty-Grotto", s:"North-Woods-Path-A", w:"none", e:"none"},
-  descriptions:{light:"The interior of the forest is shady, but well lit with dappled sunshine falling through the canopy. Birds call out to each other, unseen in the high tree-tops. North of you, you can see a brighter patch of light.", dark:"It's almost pitch-black inside the wood, but you can just make out the path in front of you. There seems to be a patch of moonlight to the north. Do the night noises of the forest seem quieter here?"},
-  },
-  "Empty-Grotto":{exits:
-  {n:"none", s:"North-Woods-Path-B", w:"none", e:"none"},
-  descriptions:{light:"Sunlight streams down in this small clearing. The trees tower around the edges, but this roughly circular patch of land is oddly clear of even the smallest sapling. Instead, vines, grasses, and flowers cover the ground here. You hear the raucus chatter of crows, and the swish of the leaves as the breeze dips into the clearing from above.", dark:"The small clearing is dim but clear to your night-adjusted eyes, with moonlight streaming down from the break in the canopy. The rustling leaves and the sound of a few crickets are the only things you hear here."},
-  },
-  "Plains-X":{exits:
-  {n:"North-Woods-Entrance", s:"none", w:"none", e:"none"},
-  descriptions:{light:"To the east, south, and west, stretch the open plains. Sunlight streams down on the tall grasses and small bushes that populate the plains. To the north, a grand forest stands, full of great redwoods and a smattering of other conifers and deciduous trees. The undergrowth seems impassibly dense, except for the path leading north from this spot.", dark:"Around you to the east, south, and west, the moon and stars shine down on the open, windy plains, lighting the grasses and small shrubs in a grayscale symphony of motion. The forest ahead is an imposing wall of darkness, and you can just make out the trail leading in to the north."},
-  },
-};
-let emptyDirections = {n:"", s:"", w:"", e:""};
 let locationIndex = "North-Woods-Entrance";
 const shortDirections = {n:"north", s:"south", e:"east", w:"west"};
+//Game data is accessed with the variable woodsWalk
 
 
 
@@ -82,15 +60,15 @@ const Chatroom = function(direction) {
   if (direction === "start") {
     locationIndex = "North-Woods-Entrance";
     $("#anchor").before(`<p class="displayed-message" style="color:rgb(249, 255, 199)">In: ${locationIndex.replace(/ /g, " ")}</p>`);
-    $("#anchor").before(`<p class="displayed-description">${locations[locationIndex].descriptions["light"]}</p>`);
+    $("#anchor").before(`<p class="displayed-description">${woodsWalk[locationIndex].descriptions["light"]}</p>`);
     updateScroll();
-  } else if (!(locations[locationIndex].exits[direction] === "none")) {
+  } else if (!(woodsWalk[locationIndex].exits[direction] === "none")) {
     //unsubscribe from previous room
-    //set locationIndex to next locations
-    locationIndex = locations[locationIndex].exits[direction];
+    //set locationIndex to next location
+    locationIndex = woodsWalk[locationIndex].exits[direction];
     $("#anchor").before(`<p class="displayed-message">${pubnub.getUUID()} moved ${shortDirections[direction]}</p>`);
     $("#anchor").before(`<p class="displayed-message" style="color:rgb(249, 255, 199)">In: ${locationIndex.replace(/ /g, " ")}</p>`);
-    $("#anchor").before(`<p class="displayed-description">${locations[locationIndex].descriptions["light"]}</p>`);
+    $("#anchor").before(`<p class="displayed-description">${woodsWalk[locationIndex].descriptions["light"]}</p>`);
     updateScroll();
 
   }
@@ -151,17 +129,23 @@ $("#submit-button").click(function(event) {
 
   //accept look (look or l)
   if (value.toLowerCase().startsWith("l ") || (value.toLowerCase().startsWith("l") && value.length === 1) || value.toLowerCase().startsWith("look ") || (value.toLowerCase().startsWith("look") && value.length === 4)){
-    $("#anchor").before(`<p class="displayed-message" style="color:rgb(249, 255, 199)">In: ${locationIndex.replace(/ /g, " ")}</p>`);
-    $("#anchor").before(`<p class="displayed-description">${locations[locationIndex].descriptions["light"]}</p>`);
+    $("#anchor").before(`<p class="displayed-message" style="color:rgb(249, 255, 199)">You look around you.</p>`);
+    $("#anchor").before(`<p class="displayed-description">${woodsWalk[locationIndex].descriptions["light"]}</p>`);
     updateScroll();
 
     //making slicer for speaking cues
   } else if (value.startsWith("\"") || value.startsWith("\'") || value.toLowerCase().startsWith("say ")){
+    let startsWithQuotes = false;
+    if (value.toLowerCase().startsWith("say ")) {
+      value = value.slice(4);
+    }
     if (value.startsWith("\"") || value.startsWith("\'")) {
       value = value.slice(1);
+      startsWithQuotes = true;
     }
-    if (value.endsWith("\"") || value.endsWith("\'")) {
+    if ((value.endsWith("\"") || value.endsWith("\'")) && (startsWithQuotes)) {
       value = value.slice(0,-1);
+      startsWithQuotes = false;
     }
     
     publishMessage(value);
